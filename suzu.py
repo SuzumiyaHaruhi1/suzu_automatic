3a4379a79f61c9df77888bbd7df24acbfe0fb37d
SuzumiyaHaruhi1 <84810190+SuzumiyaHaruhi1@users.noreply.github.com>
2024-06-13 17:47:09 +0300

    Update suzu.py

--- suzu.py
+++ suzu.py
@@ -21,43 +21,62 @@ def run_seth(command, interface=None, subnet=None, server=None, ip_address=None)
 def run_kerberoasting(ip, username, credentials, domain, wordlist):
     subprocess.run(['python3', 'suzu_kerberoasting.py', ip, '-u', username, '-c', credentials, '-d', domain, '-w', wordlist])
 
+def run_asreproasting(dc_ip, domain, usersfile=None, username=None, credentials=None, wordlist="/usr/share/wordlists/rockyou.txt"):
+    args = ['python3', 'suzu_asreproasting.py', dc_ip, '-d', domain, '-w', wordlist]
+
+    if usersfile:
+        args.extend(['-f', usersfile])
+    elif username and credentials:
+        args.extend(['-u', username, '-c', credentials])
+
+    subprocess.run(args)
+
 def main():
-    parser = argparse.ArgumentParser(description='Скрипт для автоматизации запуска доступных модулей')
-    
-    subparsers = parser.add_subparsers(dest='script', help='Выбор скрипта для запуска')
+    parser = argparse.ArgumentParser(description='Script for automating available modules')
+
+    subparsers = parser.add_subparsers(dest='script', help='Choose script to execute')
 
     # Kyocera parser
-    parser_kyocera = subparsers.add_parser('kyocera', help='Запуск модуля KYOCERA')
-    parser_kyocera.add_argument('-i', '--interface', required=True, help='Сетевой интерфейс')
-    parser_kyocera.add_argument('-s', '--subnet', required=True, help='Подсеть или одиночный IP для сканирования')
+    parser_kyocera = subparsers.add_parser('kyocera', help='Run KYOCERA module')
+    parser_kyocera.add_argument('-i', '--interface', required=True, help='Network interface')
+    parser_kyocera.add_argument('-s', '--subnet', required=True, help='Subnet or single IP to scan')
 
     # GoWitness parser
-    parser_gowitness = subparsers.add_parser('gowitness', help='Запуск модуля GOWITNESS')
-    parser_gowitness.add_argument('-i', '--interface', required=True, help='Сетевой интерфейс')
-    parser_gowitness.add_argument('-s', '--subnet', required=True, help='Подсеть или одиночный IP для сканирования')
+    parser_gowitness = subparsers.add_parser('gowitness', help='Run GOWITNESS module')
+    parser_gowitness.add_argument('-i', '--interface', required=True, help='Network interface')
+    parser_gowitness.add_argument('-s', '--subnet', required=True, help='Subnet or single IP to scan')
 
     # SETH parser
-    parser_seth = subparsers.add_parser('seth', help='Запуск модуля SETH')
-    seth_subparsers = parser_seth.add_subparsers(dest='command', help='Доступные команды')
+    parser_seth = subparsers.add_parser('seth', help='Run SETH module')
+    seth_subparsers = parser_seth.add_subparsers(dest='command', help='Available commands')
 
-    mitm_parser = seth_subparsers.add_parser('mitm', help='Запуск SETH на указанную подсеть')
-    mitm_parser.add_argument('-i', '--interface', required=True, help="Сетевой интерфейс")
-    mitm_parser.add_argument('-s', '--subnet', required=True, help="Подсеть или одиночный IP для прослушивания")
-    mitm_parser.add_argument('-r', '--server', required=True, help="RDP сервер")
+    mitm_parser = seth_subparsers.add_parser('mitm', help='Run SETH in MITM mode')
+    mitm_parser.add_argument('-i', '--interface', required=True, help="Network interface")
+    mitm_parser.add_argument('-s', '--subnet', required=True, help="Subnet or single IP to listen on")
+    mitm_parser.add_argument('-r', '--server', required=True, help="RDP server")
 
-    seth_subparsers.add_parser('show', help='Вывод всех паролей')
-    seth_subparsers.add_parser('clear', help='Очистка базы данных')
+    seth_subparsers.add_parser('show', help='Show all passwords')
+    seth_subparsers.add_parser('clear', help='Clear the database')
 
-    remove_parser = seth_subparsers.add_parser('remove', help='Удаление определенного IP-адреса')
-    remove_parser.add_argument('ip', help="IP-адрес для удаления")
+    remove_parser = seth_subparsers.add_parser('remove', help='Remove specific IP address')
+    remove_parser.add_argument('ip', help="IP address to remove")
 
     # Kerberoasting parser
-    parser_kerberoasting = subparsers.add_parser('kerberoasting', help='Запуск модуля KERBEROASTING')
-    parser_kerberoasting.add_argument('ip', help="IP-адрес контроллера домена")
-    parser_kerberoasting.add_argument('-u', '--username', required=True, help="Имя пользователя")
-    parser_kerberoasting.add_argument('-c', '--credentials', required=True, help="Пароль или NT-hash")
-    parser_kerberoasting.add_argument('-d', '--domain', required=True, help="Домен")
-    parser_kerberoasting.add_argument('-w', '--wordlist', default='/usr/share/wordlists/rockyou.txt', help="Путь к файлу со словарем")
+    parser_kerberoasting = subparsers.add_parser('kerberoasting', help='Run KERBEROASTING module')
+    parser_kerberoasting.add_argument('ip', help="Domain controller IP address")
+    parser_kerberoasting.add_argument('-u', '--username', required=True, help="Username")
+    parser_kerberoasting.add_argument('-c', '--credentials', required=True, help="Password or NT-hash")
+    parser_kerberoasting.add_argument('-d', '--domain', required=True, help="Domain")
+    parser_kerberoasting.add_argument('-w', '--wordlist', default='/usr/share/wordlists/rockyou.txt', help="Path to wordlist file")
+
+    # Asreproasting parser
+    parser_asreproasting = subparsers.add_parser('asreproasting', help='Run ASREPROASTING module')
+    parser_asreproasting.add_argument('dc_ip', type=str, help="Domain controller IP address")
+    parser_asreproasting.add_argument('-d', '--domain', type=str, required=True, help="Domain")
+    parser_asreproasting.add_argument('-f', '--usersfile', type=str, help="File with users")
+    parser_asreproasting.add_argument('-u', '--username', type=str, help="Username")
+    parser_asreproasting.add_argument('-c', '--credentials', type=str, help="Password or NT-hash")
+    parser_asreproasting.add_argument('-w', '--wordlist', type=str, default="/usr/share/wordlists/rockyou.txt", help="Path to wordlist")
 
     args = parser.parse_args()
 
@@ -97,6 +116,18 @@ def main():
             parser_kerberoasting.print_help(sys.stderr)
         else:
             run_kerberoasting(args.ip, args.username, args.credentials, args.domain, args.wordlist)
+    elif args.script == 'asreproasting':
+        if args.username or args.credentials:
+            if args.usersfile:
+                parser_asreproasting.error("-f/--usersfile cannot be used with -u/--username or -c/--credentials")
+        elif args.usersfile:
+            if not (args.username and args.credentials):
+                parser_asreproasting.error("-u/--username and -c/--credentials must both be provided")
+
+        if not args.dc_ip or not args.domain:
+            parser_asreproasting.print_help(sys.stderr)
+        else:
+            run_asreproasting(args.dc_ip, args.domain, args.usersfile, args.username, args.credentials, args.wordlist)
 
 if __name__ == '__main__':
     main()
